5bb2a31c9cf5621d31adfba2a3b05a0ef389d556
nvdong1108 <nguyenvandong1108@gmail.com>
2024-04-21 22:39:31 +0700

    add api key

--- src/main/java/com/binance/connector/futures/config/PrivateConfig.java
+++ src/main/java/com/binance/connector/futures/config/PrivateConfig.java
@@ -5,8 +5,8 @@ public final class PrivateConfig {
     }
     public static final String UM_BASE_URL = "https://fapi.binance.com";
     public static final String CM_BASE_URL = "https://dapi.binance.com";
-    public static final String API_KEY = "";
-    public static final String SECRET_KEY = "";
+    public static final String API_KEY = "afZzAgNGH5Mq1dEx5fguELTbY4JZ5n9pf5WNfAQu8rWDiPQUjVxho3SHZ2BLiDSx";
+    public static final String SECRET_KEY = "pjaF2LZ6vRIcpEQMcycGWFIwsc0ctdwRRda7tJ5um22OHR1ztjXX4JwbLF26dRyN";
 
     public static final String TESTNET_BASE_URL = "https://testnet.binancefuture.com";
     public static final String TESTNET_API_KEY = "0094379c20fe9b9db823f7d6594c7065f89fd34e20064d54b199416ae7c5fbf5";
