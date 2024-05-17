b49f63099f7147e446ea6972555423125795d3c3
nvdong1108 <nguyenvandong1108@gmail.com>
2024-05-17 16:56:35 +0700

    fix

--- src/main/java/com/binance/connector/futures/service/FirebaseInitialization.java
+++ src/main/java/com/binance/connector/futures/service/FirebaseInitialization.java
@@ -6,6 +6,8 @@ import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.google.auth.oauth2.GoogleCredentials;
@@ -17,20 +19,24 @@ public class FirebaseInitialization {
 
     private final static Logger logger = LoggerFactory.getLogger(FirebaseInitialization.class);
 
+    @Autowired
+    Environment environment;
+
     @PostConstruct
     public void initialization(){
         try {
-            FileInputStream serviceAccount = new FileInputStream("./key-firebase.json");
-    
+
+            FileInputStream serviceAccount = null;
+            if("prod".equals(environment.getProperty("spring.profiles.active"))){
+                serviceAccount=new FileInputStream("./key-firebase.json");
+            }else {
+                serviceAccount=new FileInputStream("./test-firebase.json");
+            }
             @SuppressWarnings("deprecation")
             FirebaseOptions options = new FirebaseOptions.Builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
             .build();
             FirebaseApp.initializeApp(options);
-            // if (!FirebaseApp.getApps(this).isEmpty()){
-            //         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-            // }
-           
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
